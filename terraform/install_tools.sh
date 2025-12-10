a806f7b6b9c860dbf21f256ca3a9eef502f1b957
nilesh-fatfatwale <fatfatwalenilesh@gmail.com>
2025-12-10 18:07:58 +0530

    Changes

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update system and install core packages
-sudo apt update
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y fontconfig openjdk-17-jre 
 
 # Jenkins installation
@@ -35,10 +35,22 @@ sudo apt-get update -y
 sudo apt-get install trivy -y
 
 # AWS CLI installation
-sudo snap install aws-cli --classic
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install unzip
+unzip -q awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update   
 
 # Helm installation
-sudo snap install helm --classic
+sudo apt-get install curl gpg apt-transport-https --yes
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 
 # Kubectl installation
-sudo snap install kubectl --classic
+curl -LO https://dl.k8s.io/release/v1.34.0/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
