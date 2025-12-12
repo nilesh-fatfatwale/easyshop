812fcc32675f76f847e1776df1258596e29bd4f2
Nilesh Fatfatwale <89182735+nilesh-fatfatwale@users.noreply.github.com>
2025-12-12 17:43:44 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.18.1"
+
+  name            = local.name
+  cidr            = local.vpc_cidr
+  azs             = local.azs
+  public_subnets  = local.public_subnets
+  private_subnets = local.private_subnets
+
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+
+  # Ensure public subnets auto-assign public IPs
+  map_public_ip_on_launch = true
+
+}
