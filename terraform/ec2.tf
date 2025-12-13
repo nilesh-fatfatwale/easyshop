7daa0023a83e29269a08a5fc0c78e61b2690ba45
nilesh-fatfatwale <fatfatwalenilesh@gmail.com>
2025-12-13 11:07:52 +0530

    added ports

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -28,7 +28,8 @@ resource "aws_security_group" "allow_user_to_connect" {
       { description = "port 80 allow", from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
       { description = "port 443 allow", from_port = 443, to_port = 443, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
       { description = "port 8080 allow", from_port = 8080, to_port = 8080, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
-      { description = "port 9000 allow", from_port = 9000, to_port = 9000, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
+      { description = "port 9000 allow", from_port = 9000, to_port = 9000, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] },
+      { description = "port 9100 allow", from_port = 9100, to_port = 9100, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"] }
     ]
     content {
 
